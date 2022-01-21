3e95d328e9cedfd626151df72d3338bbd2365982
Dmitriy <dima06444@gmail.com>
2022-01-21 18:39:33 +0200

    4.0

--- js/script.js
+++ js/script.js
@@ -17,3 +17,62 @@ percents.forEach((item, i) =>{
     lines[i].style.width = item.innerHTML;
 });
 
+
+
+(function($) {
+    $(function() {
+      $('.modal__close').on('click', function() {
+        $('.overlay, #thanks').fadeOut('slow');
+      });
+        $('form').validate({
+            rules:{
+                name:  {
+                  required: true,
+                  minlength: 2
+                },
+                // text: "required",
+                email: {
+                  required: true,
+                  email: true
+                },
+                check: {
+                    required: true
+                }
+            },
+            messages: {
+              name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+              },
+              check: {
+                required: "Нужно дать согласия",
+              }
+            }
+            });
+        $('form').submit(function(e) {
+            e.preventDefault();
+      
+            if(!$(this).valid()) {
+              return;
+            }
+      
+            $.ajax({
+              type: "POST",
+              url: "mailer/smart.php",
+              data: $(this).serialize()
+            }).done(function() {
+              $(this).find("input").val("");
+              // $('#consultation, #order').fadeOut();
+              $('.overlay, #thanks').fadeIn('slow');
+              
+              $('form').trigger('reset');
+            });
+            return false;
+          });
+});
+})(jQuery);
+
